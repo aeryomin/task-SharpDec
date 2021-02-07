956d531a6ce7fb3375eb10c0913faef08eadaf4c
aeryomin <eryomin.developer@gmail.com>
2021-02-07 22:08:40 +0300

    made logout

--- client/pages/presentational/Main.jsx
+++ client/pages/presentational/Main.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Cookies from 'universal-cookie'
+import Button, { DO_FUNCTION } from '../../components/Button'
+import { history } from '../../redux'
+
+const logout = () => {
+  const cookie = new Cookies()
+  history.push('/login')
+  window.location.reload()
+  cookie.remove('token')
+}
 
 const Main = () => {
-  return <div>Main</div>
+  return (
+    <div>
+      Main
+      <Button
+        content="Log out"
+        action={{ type: DO_FUNCTION, payload: logout }}
+      />
+    </div>
+  )
 }
 
 export default Main
