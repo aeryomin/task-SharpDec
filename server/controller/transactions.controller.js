c77274b2cfc054b3abe3eb6293a5281ea11cbf80
aeryomin <eryomin.developer@gmail.com>
2021-02-11 00:38:51 +0300

    made transaction between users

--- server/controller/transactions.controller.js
+++ server/controller/transactions.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import jwt from 'jsonwebtoken'
 import Account from '../model/Account.model'
 import Transactions from '../model/Transactions.model'
@@ -18,14 +19,42 @@ export async function createTransaction(req, res) {
 
   try {
     const jwtAccount = jwt.verify(req.cookies.token, config.secret)
-    const account = await Account.findById(jwtAccount.uid)
 
-    console.log('account', account)
+    const account = await Account.findById(jwtAccount.uid)
+    console.log('account: ', account)
+
     const accountTransactions = await Transactions.findOne({
       transactionToken: account.transactionToken
     })
+    console.log('accountTransactions: ', accountTransactions)
 
-    console.log(accountTransactions)
+    const recipient = await Account.findById(req.body.recipientId)
+    console.log('recipient: ', recipient)
+
+    const recipientTransactions = await Transactions.findOne({
+      transactionToken: recipient.transactionToken
+    })
+
+    if (Number(req.body.amount) < Number(accountTransactions.currentBalance)) {
+      const newRecipientBalance =
+        Number(recipientTransactions.currentBalance) + Number(req.body.amount)
+
+      const newAccountBalance =
+        Number(accountTransactions.currentBalance) - Number(req.body.amount)
+
+      await Transactions.updateOne(
+        { transactionToken: recipientTransactions.transactionToken },
+        { $set: { currentBalance: newRecipientBalance } },
+        { upsert: false }
+      )
+      await Transactions.updateOne(
+        { transactionToken: accountTransactions.transactionToken },
+        { $set: { currentBalance: newAccountBalance } },
+        { upsert: false }
+      )
+    } else {
+      res.status(400).send('Balance exceeded')
+    }
 
     res.json(accountTransactions)
   } catch (err) {
